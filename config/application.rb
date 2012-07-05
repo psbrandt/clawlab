b86552d46a966adc998e7d60953a0d6bd44db911
Gabriel Cardoso <gabriel.cardoso@inria.fr>
2012-07-05 15:54:52 +0200

    models/actions directory is now loaded renamed action_spec

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Claw
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{Rails.root}/lib)
     config.autoload_paths += %W(#{Rails.root}/app/models/requests)
+    config.autoload_paths += %W(#{Rails.root}/app/models/actions)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.

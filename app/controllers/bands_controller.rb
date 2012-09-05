908e85d8866e0c4a7a09ea9188bc7c06cfbf895d
Gabriel Cardoso <gabriel.cardoso@inria.fr>
2012-09-05 19:51:34 +0200

    Moved to bootstrap, changed views

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -4,13 +4,27 @@ class BandsController < ApplicationController
   def create
     respond_to do |format|
       if @band.save!
-        format.html { redirect_to user_bands_url, :notice => "Band successfully created" }
+        format.html { redirect_to user_bands_url(current_user), :notice => "Band successfully created" }
+        format.json { render :json => {:message => "Song version was successfully deleted"} }
       else
         format.html { render :action => "new" }
       end
     end
   end
   
+  def destroy
+    respond_to do |format|
+      begin 
+        @band.destroy
+        format.html {redirect_to user_bands_url(current_user), 
+          :notice => "Band was successfully deleted"}
+        format.json { render :json => {:message => "Band was successfully deleted"} }
+      rescue
+        not_found
+      end
+    end
+  end
+
   def index
     @bands = @bands.where(:user_ids => params[:user_id]) if params[:user_id]
   end

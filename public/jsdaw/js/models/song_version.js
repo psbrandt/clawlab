ad51852aabe0fa4b12b116b2ea8e491636e865fb
Gabriel Cardoso <gabriel.cardoso@inria.fr>
2012-09-10 19:24:42 +0200

    Added add-track button, track model and collection

--- public/jsdaw/js/models/song_version.js
+++ public/jsdaw/js/models/song_version.js
@@ -2,8 +2,19 @@ define([
   "jquery",
   "underscore",
   "backbone",
-], function($, _, Backbone) {
+  "collections/track_collection",
+  "models/track"
+], function($, _, Backbone, TrackCollection, Track) {
   var SongVersion = Backbone.Model.extend ({
+    initialize : function (data) {
+      // initializing an array of TrackModel from JSON data
+      var trackModels = _.map(data.tracks, function (json_track) { 
+        new Track (json_track) 
+      });
+
+      this.tracks = new TrackCollection (trackModels);
+    }
+    
   });
 
   return SongVersion;
--- public/jsdaw/js/views/transport.js
+++ public/jsdaw/js/views/transport.js
@@ -2,15 +2,27 @@ define([
   "jquery",
   "underscore",
   "backbone",
+  "models/track", 
   "text!templates/transport.html"
-], function($, _, Backbone, transportT) {
+], function($, _, Backbone, Track, transportT) {
   var transportView = Backbone.View.extend ({
+
+    events : {
+      "click #add-track-button" : "addTrackClicked"
+    },
+
     render : function () {
       var data = {
         title : this.model.get("title")
       };
       var template = _.template (transportT, data);
-      $("body").append (template);
+      $(this.el).html(template);
+      $("body").append (this.el);
+      return this;
+    },
+
+    addTrackClicked : function () {
+      this.model.tracks.add(new Track);
     }
   });
   
